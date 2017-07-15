f16c2350ac491e7223dc726c9b06687bc2ac84bc
Robert Sorkin <robasorkin@gmail.com>
2017-07-15 17:19:42 +0300

    test-b

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/abc', function(req, res) {
 		res.send('bad');
 	});
 });
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
 	http.listen(PORT, function() {
 		console.log('Express server listening on port ' + PORT);
 	});
