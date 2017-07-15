08c9a5d8c22ed42189d3856ca08dfc38a1dfef6c
Robert Sorkin <robasorkin@gmail.com>
2017-07-15 18:54:25 +0300

    test-f

--- middleware.js
+++ middleware.js
@@ -2,6 +2,29 @@ var cryptojs = require('crypto-js');
 var _ = require('underscore');
 var cookieParser = require('cookie-parser');
 
+function authenticateU(body) {
+	return new Promise(function(resolve, reject) {
+		if (!_.isString(body.username) || !_.isString(body.password)) {
+			console.log('not string ' + body.password + body.username);
+			return reject();
+		}
+		user.findOne({
+			where: {
+				username: body.username
+			}
+		}).then(function(user) {
+			if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+				console.log('password error');
+				return reject();
+			}
+			resolve(user);
+
+
+		}, function(e) {
+			reject();
+		});
+	});
+}
 
 module.exports = function(db) {
 	return {
@@ -34,7 +57,7 @@ module.exports = function(db) {
 		validCheck: function(req, res, next) {
 			var body = _.pick(req.body, 'username', 'password');
 			console.log(db);
-			db.user.authenticate(body).then(function(user) {
+			authenticateU(body).then(function(user) {
 				if (user != null && user.valid == true) {
 					req.user = user;
 					next();
