e4117fb86cd316c4e4a19180c8cff51ca50d2fdc
Robert Sorkin <robasorkin@gmail.com>
2017-07-16 13:49:21 +0300

    tokenGen

--- server.js
+++ server.js
@@ -516,7 +516,9 @@ io.on('connection', function(socket) {
 	});
 });
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	http.listen(PORT, function() {
 		console.log('Express server listening on port: ' + PORT);
 	});
