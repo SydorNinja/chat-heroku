8142074ee0c2c87c79af40400c229477e1276654
Robert Sorkin <robasorkin@gmail.com>
2017-08-01 20:23:51 +0300

    try-ui

--- public/js/app.js
+++ public/js/app.js
@@ -545,6 +545,8 @@ if (window.location.pathname != '/index.html' && window.location.pathname != '/'
 }
 
 socket.on('myUsername', function(username) {
+	console.log(1);
+	console.log(username);
 	$('#my-username').html(username);
 });
 
